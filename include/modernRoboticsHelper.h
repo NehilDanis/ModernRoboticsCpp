0ce2d2961832f3fa6c53af36c9392c19d6fc5879
Nehil Danış <nehild@outlook.com>
2023-04-27 01:35:50 +0200

    ADDED: cmake file along with the source and includes

---
+++ include/modernRoboticsHelper.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace mrCpp {
+    void printSomething();
+}
---
+++ src/modernRoboticsHelper.cpp
@@ -0,0 +1,8 @@
+#include "modernRoboticsHelper.h"
+#include <iostream>
+
+namespace mrCpp {
+    void printSomething() {
+        std::cout << "Hello from Lib" << std::endl;
+    }
+}
