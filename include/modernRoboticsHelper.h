29917fc10a58e6fb4936e9823a555d3bded12f10
Nehil Danış <nehild@outlook.com>
2023-06-02 23:04:11 +0200

    ADDED: transformation inverse function

--- include/modernRoboticsHelper.h
+++ include/modernRoboticsHelper.h
@@ -1,5 +1,20 @@
 #pragma once
 
+#include <eigen3/Eigen/Dense>
+
+typedef Eigen::Matrix<double, 6, 1> Vector6d;
+
 namespace mrCpp {
     void printSomething();
+
+    /**
+     * @brief Given a 4x4 transformation matrix, returns the inverse of it.
+     * 
+     * @param transformation 4x4 transformation matrix
+     */
+    Eigen::Matrix4d TransInv(const Eigen::Matrix4d& transformation);
+
+    Eigen::Matrix3d VecTose3(const Vector6d& twist);
+
+    
 }
--- src/modernRoboticsHelper.cpp
+++ src/modernRoboticsHelper.cpp
@@ -5,4 +5,14 @@ namespace mrCpp {
     void printSomething() {
         std::cout << "Hello from Lib" << std::endl;
     }
+
+    Eigen::Matrix4d TransInv(const Eigen::Matrix4d& transformation) {
+        auto rotation = transformation.block<3, 3>(0,0);
+        auto translation = transformation.block<3, 1>(0,3);
+        Eigen::Matrix4d inverse;
+        inverse.row(3) << 0, 0, 0, 1;
+        inverse.block(0, 0, 3, 3) = rotation.transpose();
+        inverse.col(3).head(3) = -1 * rotation.transpose() * translation;
+        return inverse;
+    }
 }
